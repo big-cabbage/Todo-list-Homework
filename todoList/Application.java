a5b20e8cff1f241084dabce410523db9bf940f4d
big-cabbage <wangxintiny@gmail.com>
2019-05-16 22:11:50 +0800

    create initial boot spring project

---
+++ todoList/Application.java
@@ -0,0 +1,12 @@
+package todoList;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
